f643ec60b2907c39b32c12445d29a81086ed0e0f
Astitwa Kumar <astitwa.kumar@u2opiamobile.com>
2025-06-02 23:43:14 +0530

    Added models

--- JobPortalApp/models.py
+++ JobPortalApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_recruiter = models.BooleanField(default=False)
+
+
+class JobPosts(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    posted_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_recruiter": True}
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Application(models.Model):
+    candidate = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_recruiter": False}
+    )
+    job_post = models.ForeignKey(JobPosts, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
