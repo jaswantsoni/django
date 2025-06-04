f73b2ad01e0e3571831bbfc4480e4acee1829e9d
Astitwa Kumar <astitwa.kumar@u2opiamobile.com>
2025-06-04 23:49:57 +0530

    Created Model View Set

--- JobPortalApp/views.py
+++ JobPortalApp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from rest_framework import viewsets
+
+from JobPortalApp.models import *
+from .serializers import *
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class JobPostViewSet(viewsets.ModelViewSet):
+    queryset = JobPost.objects.all()
+    serializer_class = JobPostSerializer
+    permission_classes = [AllowAny]
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = [AllowAny]
+
+
+#  Different method for making controllers
+# @api_view(["GET"])
+# def getUser(request):
+#     users = User.objects.all()
+#     serialized = UserSerializer(users, many = True)
+#     return Response(serialized.data)
