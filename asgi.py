d68f108c8828f7c099d080eefa6a9c45a54920ef
ash8282khan <ash8282khan@gmail.com>
2025-06-16 12:00:36 +0530

    initial commit

---
+++ asgi.py
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+"""
+ASGI config for blogsite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogsite.settings")
+
+application = get_asgi_application()
+=======
+"""
+ASGI config for blogsite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogsite.settings")
+
+application = get_asgi_application()
+>>>>>>> 1fa29a38fe6e385f68a7e5903211697dacc62351
